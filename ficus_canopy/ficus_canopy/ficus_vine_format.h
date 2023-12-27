02fe59b72eea6921c371d1675c3420b37b856c2b
Shane C Mason <shane.c.mason@gmail.com>
2023-12-27 11:53:33 -0800

    Updated to most recent protocol

--- ficus_canopy/ficus_canopy/ficus_vine_format.h
+++ ficus_canopy/ficus_canopy/ficus_vine_format.h
@@ -1,3 +1,7 @@
+#ifndef _VINE_FORMAT
+#define _VINE_FORMAT
+
+#define VINE_PAYLOAD_LEN 256
 #define VINE_END 255
 #define VINE_SHELL 250  //format: VINE_SHELL[command]VINE_END
 #define VINE_SHELL_RESPONSE 249 //format: VINE_SHELL_RESPONSE[response_str]VINE_END
@@ -5,3 +9,6 @@
 #define VINE_GFX_START 247 //format: VINE_GFX_START[command_str]VINE_END
 #define VINE_KEYPRESS 225 //format: VINE_KEYPRESS[key]VINE_END
 #define VINE_KEYRELEASE 224 //format: VINE_KEYRELEASE[key]VINE_END
+#define VINE_BASE 200
+
+#endif
